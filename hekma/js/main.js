43a1e4b4bb554eba546a9c1e527d468507d2efd9
wafaa <comp.elc.cairo@gmail.com>
2017-11-07 12:45:09 +0200

    hekma

---
+++ hekma/js/main.js
@@ -0,0 +1,43 @@
+jQuery(function($) {
+
+	//Preloader
+	$(window).load(function(){
+		adjust_imgs () ;
+	});
+
+	
+////adjust the position of images 
+	$(window).resize(function(){
+	
+	adjust_imgs () ;
+	
+	});
+	
+	
+	function adjust_imgs ()
+	{
+	
+	var hh = $('#himg0').height();
+    var ww = $('#himg0').width();
+	
+	$('#himg1').css('top',0.18*hh);
+	$('#himg1').css('left',0.7*ww);
+    $('#himg1').css('width',0.18*ww);
+	
+	$('#himg2').css('top',0.57*hh);
+	$('#himg2').css('left',0.04*ww);
+    $('#himg2').css('width',0.2*ww);
+	
+	$('#himg3').css('top',0.5*hh);
+	$('#himg3').css('left',0.7*ww);
+    $('#himg3').css('width',0.18*ww);
+	
+	$('#himg4').css('top',0.7*hh);
+	$('#himg4').css('left',0.35*ww);
+    $('#himg4').css('width',0.22*ww);
+		
+		}
+
+	
+});
+
